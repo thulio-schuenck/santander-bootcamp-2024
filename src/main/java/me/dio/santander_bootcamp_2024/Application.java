b3c647a00cfddb19d030cabe62b09bbd94378999
Thulio Schuenck <thulio.schuenck@gmail.com>
2024-06-18 18:52:32 -0300

    first commit

---
+++ src/main/java/me/dio/santander_bootcamp_2024/Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bootcamp_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
